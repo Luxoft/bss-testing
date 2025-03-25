92b37819557ce4255be3c93c17c4fd4e55384c6d
DMarkovkin <dmarkovkin@luxoft.com>
2025-03-25 16:33:12 +0100

    rename theory attribute

--- xunit/src/Bss.Testing.Xunit/Sdk/BssTheoryAttribute.cs
+++ xunit/src/Bss.Testing.Xunit/Sdk/BssTheoryAttribute.cs
@@ -5,6 +5,6 @@ namespace Bss.Testing.Xunit.Sdk;
 
 [XunitTestCaseDiscoverer("Bss.Testing.Xunit.Sdk.TheoryDiscoverer", "Bss.Testing.Xunit")]
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public class TheoryAttribute : FactAttribute
+public class BssTheoryAttribute : FactAttribute
 {
 }
